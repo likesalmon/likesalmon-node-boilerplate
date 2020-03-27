54a0b0874f2806ca4ec0084b4ea3209154dad965
Ammon Morris <ammon.morris@soul-cycle.com>
2020-03-27 12:49:16 -0600

    build(plopfile): formatting

--- plopfile.js
+++ plopfile.js
@@ -1,34 +1,32 @@
-const { PROJECT_NAMESPACE } = process.env;
-
 module.exports = plop => {
-  plop.setGenerator("module", {
-    description: "Create a module.",
+  plop.setGenerator('module', {
+    description: 'Create a module.',
     prompts: [
       {
-        type: "input",
-        name: "moduleName",
-        message: "Module Name",
-        default: "my-module"
+        type: 'input',
+        name: 'moduleName',
+        message: 'Module Name',
+        default: 'my-module',
       },
       {
-        type: "input",
-        name: "description",
-        message: "Description",
+        type: 'input',
+        name: 'description',
+        message: 'Description',
         default:
-          "Deploys a fleet of winged unicorns to battle the forces of darkness."
-      }
+          'Deploys a fleet of winged unicorns to battle the forces of darkness.',
+      },
     ],
     actions: [
       {
-        type: "add",
-        path: "src/{{moduleName}}/index.js",
-        templateFile: "internals/plop-templates/module/index.js.hbs"
+        type: 'add',
+        path: 'src/{{moduleName}}/index.js',
+        templateFile: 'internals/plop-templates/module/index.js.hbs',
       },
       {
-        type: "add",
-        path: "src/{{moduleName}}/tests/index.test.js",
-        templateFile: "internals/plop-templates/module/index.test.js.hbs"
-      }
-    ]
+        type: 'add',
+        path: 'src/{{moduleName}}/tests/index.test.js',
+        templateFile: 'internals/plop-templates/module/index.test.js.hbs',
+      },
+    ],
   });
 };
